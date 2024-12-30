f0840decb1783c6bcc91dc5446631c99d9755ca1
91989 <120821196+wveedd@users.noreply.github.com>
2024-12-30 23:41:14 +0530

    First Commit

---
+++ CerVer/src/main/java/com/example/CerVer/CerVerApplication.java
@@ -0,0 +1,13 @@
+package com.example.CerVer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CerVerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CerVerApplication.class, args);
+	}
+
+}
